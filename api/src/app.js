3a1851586a6aab76d633d458f3f7e76ace9d45cd
Camila Alvarez <alvarez.camila.alexandra@gmail.com>
2021-06-16 23:08:37 -0300

    *

--- api/src/app.js
+++ api/src/app.js
@@ -15,9 +15,8 @@ server.use(bodyParser.urlencoded({extended: false}));
 server.use(bodyParser.json({limit: '50mb'}));
 server.use(cookieParser());
 server.use(morgan('dev'));
-//server.use("/dogs", multer({storage, fileFilter, limits}).single("image"));
 server.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', FRONTEND_URL); // update to match the domain you will make the request from
+	res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
 	res.header('Access-Control-Allow-Credentials', 'true');
 	res.header(
 		'Access-Control-Allow-Headers',
