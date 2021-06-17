ec0cafe942398b1939756d69d80554ea606b39d4
Camila Alvarez <alvarez.camila.alexandra@gmail.com>
2021-06-17 00:21:20 -0300

    sd

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -1,33 +1,37 @@
-const { DataTypes } = require('sequelize');
+const {DataTypes} = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('dog', {
-    id: {
-      type: DataTypes.UUID,
-      primaryKey: true
-    },
-    name: {
-      type: DataTypes.CITEXT,
-      allowNull: false,
-      unique: true
-    },
-    height: {
-      type: DataTypes.STRING,
-      allowNull: false,     
-    },
-    weight: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    life_span: {
-      type: DataTypes.STRING,
-      defaultValue: "10 - 13 (promedio)"
-    }
-  }, {
-     freezeTableName: true, 
-     tableName: 'dog',
-     timestamps: false
-  });
+	// defino el modelo
+	sequelize.define(
+		'dog',
+		{
+			id: {
+				type: DataTypes.UUID,
+				primaryKey: true,
+			},
+			name: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				unique: true,
+			},
+			height: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			weight: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			life_span: {
+				type: DataTypes.STRING,
+				defaultValue: '10 - 13 (promedio)',
+			},
+		},
+		{
+			freezeTableName: true,
+			tableName: 'dog',
+			timestamps: false,
+		}
+	);
 };
--- api/src/models/Temperament.js
+++ api/src/models/Temperament.js
@@ -1,22 +1,26 @@
-const { DataTypes } = require('sequelize');
+const {DataTypes} = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('temperament', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    name: {
-      type: DataTypes.CITEXT,
-      allowNull: false,
-      unique: true
-    }
-  }, {
-     freezeTableName: true, 
-     tableName: 'temperament',
-     timestamps: false
-  });
+	// defino el modelo
+	sequelize.define(
+		'temperament',
+		{
+			id: {
+				type: DataTypes.INTEGER,
+				primaryKey: true,
+				autoIncrement: true,
+			},
+			name: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				unique: true,
+			},
+		},
+		{
+			freezeTableName: true,
+			tableName: 'temperament',
+			timestamps: false,
+		}
+	);
 };
