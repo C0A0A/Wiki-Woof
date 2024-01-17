146a9a21bd8a04fa4ff371ccc94d8398926bf2a9
caalvarez <73317663+C0A0A@users.noreply.github.com>
2024-01-17 14:33:49 -0300

    refactor for api breeds changes

--- api/src/controllers/dogs.js
+++ api/src/controllers/dogs.js
@@ -1,37 +1,36 @@
 'use strict';
-const {Dog, Image, Temperament} = require('../db');
+const { Dog, Image, Temperament } = require('../db');
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
-const {RAZASURL} = require('../../constants.js');
-const {API_KEY} = process.env;
 const axios = require('axios');
-const {v4: newUuid} = require('uuid');
+const { v4: newUuid } = require('uuid');
 const cloudinary = require('../cloudinary.js');
-const {bubbleSort} = require('../utils.js');
+const { bubbleSort } = require('../utils.js');
+const apiDogsBkp = require('../resources/breed_list.json')
 
 function getSomeDogs(req, res) {
 	const filter = req.query.filter ? req.query.filter : 'name';
 	const filterValue = req.query.filtervalue ? req.query.filtervalue : '';
 	const where =
 		filter === 'name'
-			? {name: {[Op.like]: `%${filterValue}%`}}
+			? { name: { [Op.like]: `%${filterValue}%` } }
 			: filter === 'weight'
-			? {weight: {[Op.like]: `%${filterValue}%`}}
-			: filter === 'height'
-			? {height: {[Op.like]: `%${filterValue}%`}}
-			: filter === 'life_span'
-			? {life_span: {[Op.like]: `%${filterValue}%`}}
-			: filter === 'image_url'
-			? {'$image.urlImage$': {[Op.like]: `%${filterValue}%`}}
-			: filter === 'temperament'
-			? {'$temperaments.name$': {[Op.like]: `%${filterValue}%`}}
-			: {};
+				? { weight: { [Op.like]: `%${filterValue}%` } }
+				: filter === 'height'
+					? { height: { [Op.like]: `%${filterValue}%` } }
+					: filter === 'life_span'
+						? { life_span: { [Op.like]: `%${filterValue}%` } }
+						: filter === 'image_url'
+							? { '$image.urlImage$': { [Op.like]: `%${filterValue}%` } }
+							: filter === 'temperament'
+								? { '$temperaments.name$': { [Op.like]: `%${filterValue}%` } }
+								: {};
 	const order =
 		req.query.orderby && !req.query.direction
 			? [req.query.orderby, 'ASC']
 			: req.query.orderby && req.query.direction
-			? [req.query.orderby, req.query.direction]
-			: ['id', 'ASC'];
+				? [req.query.orderby, req.query.direction]
+				: ['id', 'ASC'];
 	const standarOffset = req.query.offset0 ? Number(req.query.offset0) : 0;
 	const standarLimit = req.query.limit > 0 ? Number(req.query.limit) : 8;
 
@@ -39,52 +38,49 @@ function getSomeDogs(req, res) {
 		Dog.findAll({
 			where: where,
 			include: [
-				{model: Image, as: 'image', attributes: ['urlImage']},
+				{ model: Image, as: 'image', attributes: ['urlImage'] },
 				{
 					model: Temperament,
 					as: 'temperaments',
 					attributes: ['name'],
-					through: {attributes: []},
+					through: { attributes: [] },
 				},
 			],
 			order: [order],
-		}),
-		axios.get(`${RAZASURL}?${API_KEY}`),
+		})
 	])
 		.then((dogs) => {
 			let dbDogs = dogs[0].length
 				? dogs[0]
-						.map((dog) => dog.dataValues)
-						.map((dog) => {
-							return {
-								id: dog.id,
-								name: dog.name,
-								weight: dog.weight,
-								height: dog.height,
-								life_span: dog.life_span,
-								image_url: dog.image && dog.image.urlImage,
-								temperament: dog.temperaments.map((tem) => tem.name).join(', '),
-							};
-						})
+					.map((dog) => dog.dataValues)
+					.map((dog) => {
+						return {
+							id: dog.id,
+							name: dog.name,
+							weight: dog.weight,
+							height: dog.height,
+							life_span: dog.life_span,
+							image_url: dog.image && dog.image.urlImage,
+							temperament: dog.temperaments.map((tem) => tem.name).join(', '),
+						};
+					})
 				: [];
-			let apiDogs = dogs[1].data.length
-				? dogs[1].data
-						.map((dog) => {
-							return {
-								id: dog.id,
-								name: dog.name,
-								weight: dog.weight.metric,
-								height: dog.height.metric,
-								life_span: dog.life_span,
-								image_url: dog.image.url,
-								temperament: dog.temperament,
-							};
-						})
-						.filter(
-							(dog) =>
-								dog[filter] && dog[filter].toLowerCase().includes(filterValue)
-						)
-				: [];
+			let apiDogs = apiDogsBkp.map((dog) => {
+				return {
+					id: dog.id,
+					name: dog.name,
+					weight: dog.weight.metric,
+					height: dog.height.metric,
+					life_span: dog.life_span,
+					image_url: dog.image.url,
+					temperament: dog.temperament,
+				};
+			})
+				.filter(
+					(dog) =>
+						dog[filter] && dog[filter].toLowerCase().includes(filterValue)
+				)
+
 			let dogsResult = bubbleSort(
 				apiDogs.concat(dbDogs),
 				order[0],
@@ -98,31 +94,22 @@ function getSomeDogs(req, res) {
 			if (dogsResult.length < 8 || dogsResult.length < standarLimit)
 				return res.send({
 					dogs: dogsResult,
-					previousUrl: `${
-						process.env.BACKEND_URL
-					}/dogs/?filter=${filter}&filtervalue=${filterValue}&orderby=${
-						order[0]
-					}&direction=${order[1]}&offset0=${
-						standarOffset - standarLimit
-					}&limit=${standarLimit}`,
+					previousUrl: `${process.env.BACKEND_URL
+						}/dogs/?filter=${filter}&filtervalue=${filterValue}&orderby=${order[0]
+						}&direction=${order[1]}&offset0=${standarOffset - standarLimit
+						}&limit=${standarLimit}`,
 					nextUrl: null,
 				});
-			const nextUrl = `${
-				process.env.BACKEND_URL
-			}/dogs/?filter=${filter}&filtervalue=${filterValue}&orderby=${
-				order[0]
-			}&direction=${order[1]}&offset0=${
-				standarOffset + standarLimit
-			}&limit=${standarLimit}`;
+			const nextUrl = `${process.env.BACKEND_URL
+				}/dogs/?filter=${filter}&filtervalue=${filterValue}&orderby=${order[0]
+				}&direction=${order[1]}&offset0=${standarOffset + standarLimit
+				}&limit=${standarLimit}`;
 			const previousUrl = !standarOffset
 				? null
-				: `${
-						process.env.BACKEND_URL
-				  }/dogs/?filter=${filter}&filtervalue=${filterValue}&orderby=${
-						order[0]
-				  }&direction=${order[1]}&offset0=${
-						standarOffset - standarLimit
-				  }&limit=${standarLimit}`;
+				: `${process.env.BACKEND_URL
+				}/dogs/?filter=${filter}&filtervalue=${filterValue}&orderby=${order[0]
+				}&direction=${order[1]}&offset0=${standarOffset - standarLimit
+				}&limit=${standarLimit}`;
 			return res.send({
 				dogs: dogsResult,
 				nextUrl,
@@ -130,51 +117,50 @@ function getSomeDogs(req, res) {
 			});
 		})
 		.catch((err) =>
-			res.status(500).send({type: 'Internal Server Error.', error: err.message})
+			res.status(500).send({ type: 'Internal Server Error.', error: err.message })
 		);
 }
 
 function getAllDogs(req, res) {
-	Promise.all([Dog.findAll(), axios.get(`${RAZASURL}?${API_KEY}`)])
+	Promise.all([Dog.findAll()])
 		.then((dogs) => {
 			let dbDogs = dogs[0].length
 				? dogs[0]
-						.map((dog) => dog.dataValues)
-						.map((dog) => {
-							return {
-								id: dog.id,
-								name: dog.name,
-								weight: dog.weight,
-								height: dog.height,
-								life_span: dog.life_span,
-								image_url: dog.image && dog.image.urlImage,
-								temperament: dog.temperaments.map((tem) => tem.name).join(', '),
-							};
-						})
-				: [];
-			let apiDogs = dogs[1].data.length
-				? dogs[1].data.map((dog) => {
+					.map((dog) => dog.dataValues)
+					.map((dog) => {
 						return {
 							id: dog.id,
 							name: dog.name,
-							weight: dog.weight.metric,
-							height: dog.height.metric,
+							weight: dog.weight,
+							height: dog.height,
 							life_span: dog.life_span,
-							image_url: dog.image.url,
-							temperament: dog.temperament,
+							image_url: dog.image && dog.image.urlImage,
+							temperament: dog.temperaments.map((tem) => tem.name).join(', '),
 						};
-				  })
+					})
 				: [];
+			let apiDogs = apiDogsBkp.map((dog) => {
+				return {
+					id: dog.id,
+					name: dog.name,
+					weight: dog.weight.metric,
+					height: dog.height.metric,
+					life_span: dog.life_span,
+					image_url: dog.image.url,
+					temperament: dog.temperament,
+				};
+			});
+
 			return res.send(dbDogs.concat(apiDogs));
 		})
 		.catch((err) =>
-			res.status(500).send({type: 'Internal Server Error.', error: err.message})
+			res.status(500).send({ type: 'Internal Server Error.', error: err.message })
 		);
 }
 
 function addBreed(req, res) {
-	req.body = {...JSON.parse(req.body.info)};
-	const {name, height, weight, life_span} = req.body;
+	req.body = { ...JSON.parse(req.body.info) };
+	const { name, height, weight, life_span } = req.body;
 	const temperaments =
 		req.body.temperament &&
 		req.body.temperament.split(',').map((item) => item.trim());
@@ -183,31 +169,31 @@ function addBreed(req, res) {
 	if (!name || !height || !weight)
 		return res
 			.status(400)
-			.send({type: 'Bad request.', error: 'Required fields must be defined.'});
+			.send({ type: 'Bad request.', error: 'Required fields must be defined.' });
 	const newBreed = life_span
 		? {
-				id: newUuid(),
-				name,
-				height,
-				weight,
-				life_span,
-		  }
+			id: newUuid(),
+			name,
+			height,
+			weight,
+			life_span,
+		}
 		: {
-				id: newUuid(),
-				name,
-				height,
-				weight,
-		  };
+			id: newUuid(),
+			name,
+			height,
+			weight,
+		};
 	Dog.create(newBreed)
 		.then((data) => {
 			itemsCreated.push(data);
 			if (!file && !temperaments) return data;
 			if (file && !temperaments) {
-				return cloudinary.v2.uploader.upload(file.path, {folder: 'Wiki-woof'});
+				return cloudinary.v2.uploader.upload(file.path, { folder: 'Wiki-woof' });
 			}
 			let temperamentsToPromises = temperaments.map((temp) =>
 				Temperament.findOrCreate({
-					where: {name: temp},
+					where: { name: temp },
 				})
 			);
 			if (!file) {
@@ -215,7 +201,7 @@ function addBreed(req, res) {
 			} else {
 				return Promise.all(
 					[
-						cloudinary.v2.uploader.upload(file.path, {folder: 'Wiki-woof'}),
+						cloudinary.v2.uploader.upload(file.path, { folder: 'Wiki-woof' }),
 					].concat(temperamentsToPromises)
 				);
 			}
@@ -254,14 +240,14 @@ function addBreed(req, res) {
 			data instanceof Image
 				? (itemsCreated[0].dataValues.image_url = data.dataValues.urlImage)
 				: data[0] instanceof Image
-				? (itemsCreated[0].dataValues.image_url = data[0].dataValues.urlImage)
-				: (itemsCreated[0].dataValues.image_url = null);
+					? (itemsCreated[0].dataValues.image_url = data[0].dataValues.urlImage)
+					: (itemsCreated[0].dataValues.image_url = null);
 			return res.send(itemsCreated[0].dataValues);
 		})
 		.catch((err) => {
 			res
 				.status(500)
-				.send({type: 'Internal Server Error.', error: err.message});
+				.send({ type: 'Internal Server Error.', error: err.message });
 		});
 }
 
@@ -269,12 +255,12 @@ function findById(req, res) {
 	if (req.params.id.includes('-')) {
 		Dog.findByPk(req.params.id, {
 			include: [
-				{model: Image, as: 'image', attributes: ['urlImage']},
+				{ model: Image, as: 'image', attributes: ['urlImage'] },
 				{
 					model: Temperament,
 					as: 'temperaments',
 					attributes: ['name'],
-					through: {attributes: []},
+					through: { attributes: [] },
 				},
 			],
 		})
@@ -298,38 +284,33 @@ function findById(req, res) {
 			.catch((err) =>
 				res
 					.status(500)
-					.send({type: 'Internal Server Error.', error: err.message})
+					.send({ type: 'Internal Server Error.', error: err.message })
 			);
 	} else {
-		axios
-			.get(`${RAZASURL}?${API_KEY}`)
-			.then((dogs) => {
-				let dogResult =
-					dogs.data &&
-					dogs.data
-						.filter((dog) => dog.id === Number(req.params.id))
-						.map((dog) => {
-							return {
-								id: dog.id,
-								name: dog.name,
-								weight: dog.weight.metric,
-								height: dog.height.metric,
-								life_span: dog.life_span,
-								image_url: dog.image.url,
-								temperament: dog.temperament,
-							};
-						});
-				if (dogResult.length) return res.send(dogResult[0]);
-				return res.send({
-					type: 'Not found',
-					error: 'The id you are requesting does not exist',
+		try {
+			let dogResult = apiDogsBkp
+				.filter((dog) => dog.id === Number(req.params.id))
+				.map((dog) => {
+					return {
+						id: dog.id,
+						name: dog.name,
+						weight: dog.weight.metric,
+						height: dog.height.metric,
+						life_span: dog.life_span,
+						image_url: dog.image.url,
+						temperament: dog.temperament,
+					};
 				});
-			})
-			.catch((err) =>
-				res
-					.status(500)
-					.send({type: 'Internal Server Error.', error: err.message})
-			);
+			if (dogResult.length) return res.send(dogResult[0]);
+			return res.send({
+				type: 'Not found',
+				error: 'The id you are requesting does not exist',
+			});
+		} catch (error) {
+			res
+				.status(500)
+				.send({ type: 'Internal Server Error.', error: err.message })
+		}
 	}
 }
 
@@ -338,27 +319,27 @@ function getDataBaseDogs(req, res) {
 	const filterValue = req.query.filtervalue ? req.query.filtervalue : '';
 	const where =
 		filter === 'name'
-			? {name: {[Op.like]: `%${filterValue}%`}}
+			? { name: { [Op.like]: `%${filterValue}%` } }
 			: filter === 'weight'
-			? {weight: {[Op.like]: `%${filterValue}%`}}
-			: filter === 'height'
-			? {height: {[Op.like]: `%${filterValue}%`}}
-			: filter === 'life_span'
-			? {life_span: {[Op.like]: `%${filterValue}%`}}
-			: filter === 'image_url'
-			? {'$image.urlImage$': {[Op.like]: `%${filterValue}%`}}
-			: filter === 'temperament'
-			? {'$temperaments.name$': {[Op.like]: `%${filterValue}%`}}
-			: {};
+				? { weight: { [Op.like]: `%${filterValue}%` } }
+				: filter === 'height'
+					? { height: { [Op.like]: `%${filterValue}%` } }
+					: filter === 'life_span'
+						? { life_span: { [Op.like]: `%${filterValue}%` } }
+						: filter === 'image_url'
+							? { '$image.urlImage$': { [Op.like]: `%${filterValue}%` } }
+							: filter === 'temperament'
+								? { '$temperaments.name$': { [Op.like]: `%${filterValue}%` } }
+								: {};
 	Dog.findAll({
 		where: where,
 		include: [
-			{model: Image, as: 'image', attributes: ['urlImage']},
+			{ model: Image, as: 'image', attributes: ['urlImage'] },
 			{
 				model: Temperament,
 				as: 'temperaments',
 				attributes: ['name'],
-				through: {attributes: []},
+				through: { attributes: [] },
 			},
 		],
 	})
@@ -380,7 +361,7 @@ function getDataBaseDogs(req, res) {
 			return res.send(result);
 		})
 		.catch((err) =>
-			res.status(500).send({type: 'Internal Server Error.', error: err.message})
+			res.status(500).send({ type: 'Internal Server Error.', error: err.message })
 		);
 }
 
